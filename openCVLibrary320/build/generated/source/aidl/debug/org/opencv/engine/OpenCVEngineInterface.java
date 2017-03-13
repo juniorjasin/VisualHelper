0c9cfe962565e0d8fca8fc5542214acb5924eff0
juniorjasin <jasinjunior@gmail.com>
2017-03-13 15:09:35 -0300

    first commit

--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/jrjs/AndroidStudioProjects/NDKOpencvTest/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/jrjs/AndroidStudioProjects/VisualHelper/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
